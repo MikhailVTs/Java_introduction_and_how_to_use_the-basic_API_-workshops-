6a4355bdd6431540248be91a8a134ea403035093
Mikhail <Caplin.m@inbox.ru>
2023-06-02 16:34:57 +1000

    Создание списка цветов

---
+++ Seminar_6/Color.java
@@ -0,0 +1,15 @@
+public enum Color {
+
+    White,  // Белый
+    Black,  // Черный
+    Red,    // Красный
+    Yellow, // Желтый
+    Orange, // Оранжевый
+    Green,  // Зеленый
+    Blue,   // Синий
+    Purple, // Фиолетовый   
+    Pink,   // Розовый
+    Brown,  // Коричневый
+    Grey    // Серый
+    
+}
