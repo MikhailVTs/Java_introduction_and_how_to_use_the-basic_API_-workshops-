059c0fb4a3bf01f46e2ef302d45c42d03064fe81
Mikhail <Caplin.m@inbox.ru>
2023-06-02 16:36:57 +1000

    Создание списка HHD

---
+++ Seminar_6/HardDisk.java
@@ -0,0 +1,9 @@
+public enum HardDisk {
+    
+    Toshiba_1_TB_HDWL110UZSVA,
+    Western_Digital_WD5000LPLX,
+    Seagate_Barracuda_1_TB,
+    Toshiba_2_TB_HDWL120EZSTA,
+    HGST_500_GB_Travelstar_Z7K500
+
+}
