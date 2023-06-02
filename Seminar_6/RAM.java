b993708e4a2d80dcb03c7bb16fb8b7a0f82e175d
Mikhail <Caplin.m@inbox.ru>
2023-06-02 16:50:45 +1000

    Создание списка оперативно запоминающего устройства

---
+++ Seminar_6/RAM.java
@@ -0,0 +1,9 @@
+public enum RAM {
+
+    Patriot_Memory_SL_PSD22G8002S,
+    Kingston_KVR800D2S6_2G,
+    Qumo_QUM2S_2G800T6,
+    Corsair_CMSA4GX3M1A1066C7,
+    Kingston_ValueRAM_KVR16S11_8
+    
+}
